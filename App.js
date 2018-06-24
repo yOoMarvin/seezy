695b324dc24ce0334dc9a65a30fd9a72e6486ca2
yOoMarvin <marvin.messenzehl@gmail.com>
2018-06-24 07:39:41 +0200

    tab bar icons and colors

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {FlatList, View, Image, Text, ActivityIndicator, TouchableOpacity, ScrollView} from "react-native";
+import {FlatList, View, Image, Text, ActivityIndicator, TouchableOpacity, ScrollView, StyleSheet} from "react-native";
 //import {List} from "react-native-elements";
 //import firebase from 'react-native-firebase';
 import { createBottomTabNavigator } from 'react-navigation';
@@ -15,7 +15,15 @@ import Icon from 'react-native-elements';
 //firebase.database().goOnline();
 
 class Startscreen extends Component{
-
+  static navigationOptions = {
+    tabBarLabel: 'EVENT',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./resources/nav/calendar.png')}
+        style={[styles.iconCalendar, { tintColor: tintColor }]}
+      />
+    ),
+  };
   render(){
     return(
     <View style = {{flex:1, marginTop:30}}>
@@ -27,7 +35,15 @@ class Startscreen extends Component{
 }
 
 class Bookedscreen extends Component {
-
+  static navigationOptions = {
+    tabBarLabel: 'GEBUCHT',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./resources/nav/shield.png')}
+        style={[styles.iconShield, { tintColor: tintColor }]}
+      />
+    ),
+  };
   render(){
     return(
       <View style = {{flex:1, marginTop:30}}>
@@ -38,6 +54,15 @@ class Bookedscreen extends Component {
 }
 
 class Chatscreen extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'CHAT',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./resources/nav/message-square.png')}
+        style={[styles.iconMessage, { tintColor: tintColor }]}
+      />
+    ),
+  };
   render(){
     return(
       <View style = {{flex: 1, marginTop:30}}>
@@ -48,6 +73,15 @@ class Chatscreen extends Component {
 }
 
 class Profilscreen extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'PROFIL',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./resources/nav/user.png')}
+        style={[styles.iconUser, { tintColor: tintColor }]}
+      />
+    ),
+  };
   render(){
     return(
       <View style = {{flexDirection:'column', marginTop:30}}>
@@ -58,10 +92,51 @@ class Profilscreen extends Component {
 }
 
 
-export default createBottomTabNavigator({
-  Start: Startscreen,
-  Booked: Bookedscreen,
-  Chat: Chatscreen,
-  Profil: Profilscreen,
-});
+export default createBottomTabNavigator(
+  {
+    Start: {
+      screen: Startscreen
+    },
+    Booked: {
+      screen: Bookedscreen
+    },
+    Chat: {
+      screen:Chatscreen
+    },
+    Profil: {
+      screen: Profilscreen
+    },
+  },
+  {
+    initialRouteName: "Start",
+    order: ["Start", "Booked", "Chat", "Profil"],
+		animationEnabled: true,
+		tabBarOptions: {
+			activeTintColor: '#EB5757',
+			inactiveTintColor: 'grey',
+			showLabel: true,
+      showIcon: true,
+      labelStyle: {fontWeight: 'bold'}
+		}
+	}
+);
 
+
+const styles = StyleSheet.create({
+  iconCalendar: {
+    width: 18,
+    height: 20
+  },
+  iconShield: {
+    width: 18,
+    height: 22
+  },
+  iconMessage: {
+    width: 18,
+    height: 18
+  },
+  iconUser: {
+    width: 18,
+    height: 20
+  }
+});
